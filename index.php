39871a14a9cd757eb3b52b8866594e3b68defc29
Alja Zalokarsi <alja.zalokarsi@gmail.com>
2018-11-16 22:59:05 +0100

    building form

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Dateability Score</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <form method ="post" action ="main.php">
+    <h1>What is your Dateability Score?</h1>
+    <!-- Text Input -->
+    <label>First Name:</label><br>
+    <input type = "text" name ="first" placeholder ="First Name" required><br>
+    <input type ="text" name ="last" placeholder ="Last Name"><br>
+    <input type ="email" name ="email" placeholder ="youremail@email.com" required><br>
+    <!-- q1 -->
+    <label>How well can you program?</label><br>
+    <select for ="q1">
+        <option for ="q1" value ="1">Expert</option>
+        <option for ="q1" value ="2">Average</option>
+        <option for ="q1" value ="3">Below Average/No Experience</option>
+    </select><br>
+    <!-- q2 -->
+    <label>What is your ideal first date?</label><br>
+    <select for ="q1">
+        <option for ="q1" value ="1">Coffe Shop</option>
+        <option for ="q1" value ="2">Dinner and Computer Programming</option>
+        <option for ="q1" value ="3">Your House</option>
+    </select><br>
+    <!-- q3 -->
+    <label>What is your highest level of education?</label><br>
+    <select for ="q1">
+        <option for ="q1" value ="1"></option>
+        <option for ="q1" value ="2"></option>
+        <option for ="q1" value ="3"></option>
+    </select><br>
+<!-- q4 -->
+<label>Do you like sci-fi?</label><br>
+    <select for ="q1">
+        <option for ="q1" value ="1"></option>
+        <option for ="q1" value ="2"></option>
+        <option for ="q1" value ="3"></option>
+    </select><br>
+    <!-- -->
+    
+
+
+
+
+
+</body>
+</html>
